69a196415fa8931942bd02221d8be135ba5c9790
15797871606 mapeng0902 <15797871606@163.com>
2019-12-06 14:32:37 +0800

    modified localhost

--- front-end/h5/jest.config.js
+++ front-end/h5/jest.config.js
@@ -22,7 +22,7 @@ module.exports = {
   testMatch: [
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
-  testURL: 'http://192.168.2.134/',
+  testURL: 'http://localhost/',
   watchPlugins: [
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname'
