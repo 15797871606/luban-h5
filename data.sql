86ecc9d6c8a0599c00491a155473adc7bfb7ffb5
15797871606 mapeng0902 <15797871606@163.com>
2019-12-06 15:14:38 +0800

    modified

--- data.sql
+++ data.sql
@@ -1,178 +1,346 @@
-BEGIN;
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50721
+Source Host           : localhost:3306
+Source Database       : buildwebsite
+
+Target Server Type    : MYSQL
+Target Server Version : 50721
+File Encoding         : 65001
+
+Date: 2019-12-03 10:03:18
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for core_store
+-- ----------------------------
+DROP TABLE IF EXISTS `core_store`;
+CREATE TABLE `core_store` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `key` longtext,
+  `value` longtext,
+  `type` longtext,
+  `environment` longtext,
+  `tag` longtext,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of core_store
+-- ----------------------------
+
+
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50721
+Source Host           : localhost:3306
+Source Database       : buildwebsite
+
+Target Server Type    : MYSQL
+Target Server Version : 50721
+File Encoding         : 65001
+
+Date: 2019-12-03 10:03:25
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for strapi_administrator
+-- ----------------------------
+DROP TABLE IF EXISTS `strapi_administrator`;
+CREATE TABLE `strapi_administrator` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `username` longtext NOT NULL,
+  `email` longtext NOT NULL,
+  `password` longtext NOT NULL,
+  `resetPasswordToken` longtext NOT NULL,
+  `blocked` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of strapi_administrator
+-- ----------------------------
+
+
+
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50721
+Source Host           : localhost:3306
+Source Database       : buildwebsite
+
+Target Server Type    : MYSQL
+Target Server Version : 50721
+File Encoding         : 65001
+
+Date: 2019-12-03 10:03:31
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for upload_file
+-- ----------------------------
+DROP TABLE IF EXISTS `upload_file`;
+CREATE TABLE `upload_file` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `name` longtext NOT NULL,
+  `hash` longtext NOT NULL,
+  `sha256` longtext NOT NULL,
+  `ext` longtext,
+  `mime` longtext NOT NULL,
+  `size` longtext NOT NULL,
+  `url` longtext NOT NULL,
+  `provider` longtext NOT NULL,
+  `public_id` longtext,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- CREATE TABLE "core_store" -----------------------------------
-CREATE TABLE "core_store"(
-	"id" Integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"key" Text,
-	"value" Text,
-	"type" Text,
-	"environment" Text,
-	"tag" Text );
--- -------------------------------------------------------------
+-- ----------------------------
+-- Records of upload_file
+-- ----------------------------
 
-COMMIT;
 
 
 
-BEGIN;
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50721
+Source Host           : localhost:3306
+Source Database       : buildwebsite
+
+Target Server Type    : MYSQL
+Target Server Version : 50721
+File Encoding         : 65001
 
--- CREATE TABLE "strapi_administrator" -------------------------
-CREATE TABLE "strapi_administrator"(
-	"id" Integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"username" Text NOT NULL,
-	"email" Text NOT NULL,
-	"password" Text NOT NULL,
-	"resetPasswordToken" Text,
-	"blocked" Boolean );
--- -------------------------------------------------------------
+Date: 2019-12-03 10:03:36
+*/
 
-COMMIT;
+SET FOREIGN_KEY_CHECKS=0;
 
+-- ----------------------------
+-- Table structure for upload_file_morph
+-- ----------------------------
+DROP TABLE IF EXISTS `upload_file_morph`;
+CREATE TABLE `upload_file_morph` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `upload_file_id` int(20) DEFAULT NULL,
+  `related_id` int(20) DEFAULT NULL,
+  `related_type` longtext,
+  `field` longtext,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- ----------------------------
+-- Records of upload_file_morph
+-- ----------------------------
 
 
-BEGIN;
 
--- CREATE TABLE "upload_file" ----------------------------------
-CREATE TABLE "upload_file"(
-	"id" Integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"name" Text NOT NULL,
-	"hash" Text NOT NULL,
-	"sha256" Text,
-	"ext" Text,
-	"mime" Text NOT NULL,
-	"size" Text NOT NULL,
-	"url" Text NOT NULL,
-	"provider" Text NOT NULL,
-	"public_id" Text,
-	"created_at" Numeric DEFAULT CURRENT_TIMESTAMP,
-	"updated_at" Numeric DEFAULT CURRENT_TIMESTAMP );
--- -------------------------------------------------------------
+
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50721
+Source Host           : localhost:3306
+Source Database       : buildwebsite
+
+Target Server Type    : MYSQL
+Target Server Version : 50721
+File Encoding         : 65001
+
+Date: 2019-12-03 10:03:41
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for users-permissions_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `users-permissions_permission`;
+CREATE TABLE `users-permissions_permission` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `type` longtext NOT NULL,
+  `controller` longtext NOT NULL,
+  `action` longtext NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  `policy` longtext,
+  `role` int(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of users-permissions_permission
+-- ----------------------------
+
+
+
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50721
+Source Host           : localhost:3306
+Source Database       : buildwebsite
+
+Target Server Type    : MYSQL
+Target Server Version : 50721
+File Encoding         : 65001
+
+Date: 2019-12-03 10:03:45
+*/
 
-COMMIT;
+SET FOREIGN_KEY_CHECKS=0;
 
+-- ----------------------------
+-- Table structure for users-permissions_role
+-- ----------------------------
+DROP TABLE IF EXISTS `users-permissions_role`;
+CREATE TABLE `users-permissions_role` (
+  `id` int(20) NOT NULL,
+  `name` longtext NOT NULL,
+  `description` longtext,
+  `type` longtext,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- ----------------------------
+-- Records of users-permissions_role
+-- ----------------------------
 
 
 
-BEGIN;
-
--- CREATE TABLE "upload_file_morph" ----------------------------
-CREATE TABLE "upload_file_morph"(
-	"id" Integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"upload_file_id" Integer,
-	"related_id" Integer,
-	"related_type" Text,
-	"field" Text );
--- -------------------------------------------------------------
-
-COMMIT;
-
-
-
-
-
-
-
-BEGIN;
-
--- CREATE TABLE "users-permissions_permission" -----------------
-CREATE TABLE "users-permissions_permission"(
-	"id" Integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"type" Text NOT NULL,
-	"controller" Text NOT NULL,
-	"action" Text NOT NULL,
-	"enabled" Boolean NOT NULL,
-	"policy" Text,
-	"role" Integer );
--- -------------------------------------------------------------
-
-COMMIT;
-
-
-
-
-
-
-
-BEGIN;
-
--- CREATE TABLE "users-permissions_role" -----------------------
-CREATE TABLE "users-permissions_role"(
-	"id" Integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"name" Text NOT NULL,
-	"description" Text,
-	"type" Text );
--- -------------------------------------------------------------
-
-COMMIT;
-
-
-
-
-
-
-BEGIN;
-
--- CREATE TABLE "users-permissions_user" -----------------------
-CREATE TABLE "users-permissions_user"(
-	"id" Integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"username" Text NOT NULL,
-	"email" Text NOT NULL,
-	"provider" Text,
-	"password" Text,
-	"resetPasswordToken" Text,
-	"confirmed" Boolean,
-	"blocked" Boolean,
-	"role" Integer,
-	"created_at" Numeric DEFAULT CURRENT_TIMESTAMP,
-	"updated_at" Numeric DEFAULT CURRENT_TIMESTAMP );
--- -------------------------------------------------------------
-
-COMMIT;
-
-
-
-
-
-
-
-BEGIN;
-
--- CREATE TABLE "workforms" ------------------------------------
-CREATE TABLE "workforms"(
-	"id" Integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"form" Text,
-	"work" Integer,
-	"created_at" Numeric DEFAULT CURRENT_TIMESTAMP,
-	"updated_at" Numeric DEFAULT CURRENT_TIMESTAMP );
--- -------------------------------------------------------------
-
-COMMIT;
-
-
-
-
-
-
-BEGIN;
-
--- CREATE TABLE "works" ----------------------------------------
-CREATE TABLE "works"(
-	"id" Integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"title" Text,
-	"description" Text,
-	"cover_image_url" Text,
-	"pages" Text,
-	"create_time" Numeric DEFAULT CURRENT_TIMESTAMP,
-	"update_time" Numeric DEFAULT CURRENT_TIMESTAMP,
-	"is_publish" Boolean,
-	"is_template" Boolean,
-	"created_at" Numeric DEFAULT CURRENT_TIMESTAMP,
-	"updated_at" Numeric DEFAULT CURRENT_TIMESTAMP );
--- -------------------------------------------------------------
-
-COMMIT;
-
-
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50721
+Source Host           : localhost:3306
+Source Database       : buildwebsite
+
+Target Server Type    : MYSQL
+Target Server Version : 50721
+File Encoding         : 65001
+
+Date: 2019-12-03 10:03:55
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for users-permissions_user
+-- ----------------------------
+DROP TABLE IF EXISTS `users-permissions_user`;
+CREATE TABLE `users-permissions_user` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `username` longtext NOT NULL,
+  `email` longtext NOT NULL,
+  `provider` longtext,
+  `password` longtext,
+  `resetPasswordToken` longtext,
+  `confirmed` tinyint(4) DEFAULT NULL,
+  `blocked` tinyint(4) DEFAULT NULL,
+  `role` int(20) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of users-permissions_user
+-- ----------------------------
+
+
+
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50721
+Source Host           : localhost:3306
+Source Database       : buildwebsite
+
+Target Server Type    : MYSQL
+Target Server Version : 50721
+File Encoding         : 65001
+
+Date: 2019-12-03 10:04:00
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for workforms
+-- ----------------------------
+DROP TABLE IF EXISTS `workforms`;
+CREATE TABLE `workforms` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `form` longtext,
+  `work` int(20) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of workforms
+-- ----------------------------
+
+
+
+
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50721
+Source Host           : localhost:3306
+Source Database       : buildwebsite
+
+Target Server Type    : MYSQL
+Target Server Version : 50721
+File Encoding         : 65001
+
+Date: 2019-12-03 10:04:06
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for works
+-- ----------------------------
+DROP TABLE IF EXISTS `works`;
+CREATE TABLE `works` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL COMMENT '标题',
+  `description` longtext COMMENT '描述',
+  `cover_image_url` longtext COMMENT '封面图链接',
+  `pages` json DEFAULT NULL COMMENT '页面数据，主要部分',
+  `is_publish` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否已经发布',
+  `is_template` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否是模板',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of works
+-- ----------------------------
 
